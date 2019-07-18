b11dac74f0b807ea4bee41784f0bbb4d41e5ba56
Julian Hess <jhess@broadinstitute.org>
2019-07-18 13:51:30 -0400

    Add chrpos2gpos, refactor seq module
    - Decorator to automatically change reference
    - Make all objects "private"

--- seq.py
+++ seq.py
@@ -1,28 +1,44 @@
-from pyfaidx import Fasta
-import sys
+from pyfaidx import Fasta as _Fasta
+import numpy as _np
+import sys as _sys
 
 # wrap in a class because we only want to remember which reference we're using,
 # so we don't have to instantiate it every time genome_region is called
-class FA:
+class _FA:
 	def __init__(self, ref_fa_file = "/mnt/j/db/hg19/ref/hs37d5.fa"):
-		self.ref_fa_file = ref_fa_file
-		self.ref_fa_obj = Fasta(self.ref_fa_file)
+		self.__set_reference(ref_fa_file)
 
+	def __auto_reset_reference(f):
+		def inner(self, *args, ref = None, **kwargs):
+			if ref is None:
+				ref = self.ref_fa_file
+
+			if ref != self.ref_fa_file:
+				self.__set_reference(ref)
+				print("NOTE: switched reference to {}".format(ref), file = _sys.stderr)
+
+			return f(self, *args, ref = ref, **kwargs)
+		return inner
+
+	@__auto_reset_reference
 	def _genome_region(self, chr, start, end, ref = None):
-		if ref is None:
-			ref = self.ref_fa_file
-
-		if ref != self.ref_fa_file:
-			self.__set_reference(ref)
-			print("NOTE: switched reference to {}".format(ref), file = sys.stderr)
-
 		# TODO: handle implicit chr conversion better
 		return self.ref_fa_obj[str(chr)][start:end].seq
 
+	@__auto_reset_reference
+	def _chrpos2gpos(self, chr, pos, ref = None):
+		# TODO: handle implicit chr conversion better
+		#       currently, this only works with b37-style names, and assumes
+		#       chr1 comes first
+		l = _np.cumsum(_np.r_[0, self.chrlens])
+		return l[chr - 1] + pos
+
 	def __set_reference(self, ref):
 		self.ref_fa_file = ref
-		self.ref_fa_obj = Fasta(self.ref_fa_file)
+		self.ref_fa_obj = _Fasta(self.ref_fa_file)
+		self.chrlens = _np.array([len(x) for x in self.ref_fa_obj.records.values()]);
 
 # export public functions
-fa = FA();
-genome_region = fa._genome_region
+_fa = _FA();
+genome_region = _fa._genome_region
+chrpos2gpos = _fa._chrpos2gpos
