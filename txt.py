7656efae637be28e766031ac7e59783ceafbeb54
Julian Hess <jhess@broadinstitute.org>
2019-09-10 11:59:15 -0400

    Send to clipboard

--- txt.py
+++ txt.py
@@ -1,5 +1,16 @@
 import pandas as pd
+import os
+import typing
+import subprocess
 
 def parsein(X, col, regex, fields):
-	T = X[col].str.extract(regex).rename(columns = dict(zip(range(0, len(fields)), fields)));
+	T = parse(X[col], regex, fields)
 	return pd.concat([X, T], 1)
+
+def parse(X, regex, fields):
+	T = X.str.extract(regex).rename(columns = dict(zip(range(0, len(fields)), fields)));
+	return T
+
+def to_clipboard(string: str):
+	p = subprocess.Popen("xclip", stdin = subprocess.PIPE)
+	p.communicate(str.encode(string))
