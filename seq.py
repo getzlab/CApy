9b3f393304cb35617fd70aa6f3da4a18c5d4df39
Julian Hess <jhess@broadinstitute.org>
2019-10-11 13:34:46 -0400

    Initial commit of gnomAD query code

--- seq.py
+++ seq.py
@@ -1,6 +1,8 @@
 from pyfaidx import Fasta as _Fasta
 import numpy as _np
 import sys as _sys
+import mmap as _mmap
+import pandas as _pd
 
 # wrap in a class because we only want to remember which reference we're using,
 # so we don't have to instantiate it every time genome_region is called
@@ -47,3 +49,40 @@ _fa = _FA();
 genome_region = _fa._genome_region
 chrpos2gpos = _fa._chrpos2gpos
 get_chrlens = _fa._get_chrlens
+
+class _gnomad:
+	def __init__(self, gnomad_dir = "/mnt/j/db/hg19/gnomad", ref = None):
+		self.gnomad_dir = gnomad_dir
+		self.obit_idx = _pd.read_parquet(gnomad_dir + "/1bit/chr1-22.index.parquet") # TODO: update to final when we generate with chrX
+		self.ref = ref
+
+		f = open(self.gnomad_dir + "/1bit/chr1-22.bin", "rb"); # TODO: update to final when we generate with chrX
+		self.mm_1bit = _mmap.mmap(f.fileno(), 0, _mmap.MAP_SHARED, prot = _mmap.PROT_READ);
+		f.close()
+
+	def __del__(self):
+		self.mm_1bit.close()
+
+	def _query_1bit(self, ch, start, end = None):
+		if end is None:
+			start = start if not _np.isscalar(start) else [start]
+			gpos_off = [(x,
+			             *self.obit_idx.loc[
+			               _np.digitize(x, self.obit_idx["g_start"]) - 1, ["offset", "g_start"]
+			             ]
+			            ) for x in chrpos2gpos(ch, start)
+			           ]
+
+			return [self.mm_1bit[offset + int((pos - g_start)/8)] &
+			        (0x80 >> ((pos - 1) % 8)) > 0 for pos, offset, g_start in gpos_off]
+		else:
+			pass
+			#np.flatnonzero(np.unpackbits(np.frombuffer(self.mm_1bit[int(start/8):int(end/8)], dtype = np.uint8))) + 1
+
+#	def _query_1bit_raw(self, ch, start, end):
+#		[gst_off, gen_off] = [(x, np.digitize(x, self.obit_idx["g_start"]) - 1) for x in chrpos2gpos(ch, [start, end])]
+#
+#		return np.frombuffer(self.mm_1bit[int(start/8):int(end/8)], dtype = np.uint8)
+
+_gnmd = _gnomad()
+query_gnomad_1bit = _gnmd._query_1bit
