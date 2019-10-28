1c8fd6b56af61948e20d7ddedc511ef538dd9fc8
Julian Hess <jhess@broadinstitute.org>
2019-10-28 18:05:15 -0400

    Add print_full

--- txt.py
+++ txt.py
@@ -11,6 +11,10 @@ def parse(X, regex, fields):
 	T = X.str.extract(regex).rename(columns = dict(zip(range(0, len(fields)), fields)));
 	return T
 
+def print_full(D):
+	with pd.option_context('display.max_rows', None, 'display.max_columns', None):
+		print(D)
+
 def to_clipboard(string: str):
 	p = subprocess.Popen("xclip", stdin = subprocess.PIPE)
 	p.communicate(str.encode(string))
